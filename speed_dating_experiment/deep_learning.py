dafdcde99a3408beb07f44d1264d7f451d900106
Toby <Toby@MacBookPro.local>
2017-02-14 13:12:33 +0800

    remove irrelevant rows and columns

--- speed_dating_experiment/deep_learning.py
+++ speed_dating_experiment/deep_learning.py
@@ -7,9 +7,111 @@ import pandas as pd
 
 
 # import dataset
-dataset = pd.read_csv("Churn_Modelling.csv")
-X = dataset.iloc[:, 3:13].values  #column index 3 - 12 
-y = dataset.iloc[:, 13].values #Exited? 1 - true; 0 - false
+dataset = pd.read_csv("Speed Dating Data.csv")
+dataset.shape #8378, 195
+
+# preprocessing
+# 1) extract related column first
+# short form
+# pf_o_xxx - partner’s stated preference at Time 1 
+# XXX1_1 - what you look for in the opposite sex
+# XXX2_1 - What do you think the opposite sex looks for in a date?
+# XXX3_1 - How do you think you measure up (self evaluate)?
+# Attractive - att
+# Sincere - sin
+# Intelligent - int
+# Fun - fun
+# Ambitious - amb
+# Has shared interests/hobbies - sha
+
+# note:
+
+
+colnames = ["iid", "gender", "wave", "pid", "match", "samerace", "age_o", "race_o", \
+"pf_o_att", "pf_o_sin", "pf_o_int", "pf_o_fun", "pf_o_amb", "pf_o_sha", \
+"age", "field_cd", "race", "imprace", \
+"imprelig", "goal", "date", "go_out", "career_c", \
+"sports", "tvsports", "exercise", "dining", "museums", "art", "hiking", \
+"gaming", "clubbing", "reading", "tv", "theater", "movies", "concerts", \
+"music", "shopping", "yoga", "exphappy", \
+"attr1_1", "sinc1_1", "intel1_1", "fun1_1", "amb1_1", "shar1_1", \
+"attr2_1", "sinc2_1", "intel2_1", "fun2_1", "amb2_1", "shar2_1", \
+"attr3_1", "sinc3_1", "intel3_1", "fun3_1"]
+
+dataset = dataset[colnames] 
+
+# 2) check valid data
+# wave values 6 - 9 are in 1-10 scale, with different calculation, remove those rows
+dataset = dataset[(dataset['wave']<6) | (dataset['wave'] > 9)] #6816 rows
+
+# remove row with na value 
+# check is there any na value in pid column
+dataset["pid"].isnull().any()#is there any na value
+dataset["pid"].isnull().sum()# how many - 10
+dataset = dataset.dropna(subset = ['pid']) 
+
+dataset["age_o"].isnull().any() #is there any na value
+dataset["age_o"].isnull().sum() #how many - 89
+dataset = dataset.dropna(subset = ['age_o'])
+
+dataset["pf_o_att"].isnull().any() #is there any na value 
+dataset["pf_o_att"].isnull().sum() #how many - 16
+dataset = dataset.dropna(subset = ['pf_o_att'])
+
+dataset["pf_o_fun"].isnull().any() #is there any na value 
+dataset["pf_o_fun"].isnull().sum() #how many - 9
+dataset = dataset.dropna(subset = ['pf_o_fun'])
+
+dataset["pf_o_sha"].isnull().any() #is there any na value 
+dataset["pf_o_sha"].isnull().sum() #how many -22
+dataset = dataset.dropna(subset = ['pf_o_sha'])
+
+dataset["age"].isnull().any() #is there any na value 
+dataset["age"].isnull().sum() #how many -89
+dataset = dataset.dropna(subset = ['age'])
+
+dataset["field_cd"].isnull().any() #is there any na value 
+dataset["field_cd"].isnull().sum() #how many - 18
+dataset = dataset.dropna(subset = ['field_cd'])
+
+dataset["imprace"].isnull().any() #is there any na value 
+dataset["imprace"].isnull().sum() #how many - 16
+dataset = dataset.dropna(subset = ['imprace'])
+
+dataset["date"].isnull().any() #is there any na value 
+dataset["date"].isnull().sum() #how many - 18
+dataset = dataset.dropna(subset = ['date'])
+
+dataset["career_c"].isnull().any() #is there any na value 
+dataset["career_c"].isnull().sum() #how many - 38
+dataset = dataset.dropna(subset = ['career_c'])
+
+dataset["fun1_1"].isnull().any() #is there any na value 
+dataset["fun1_1"].isnull().sum() #how many -9
+dataset = dataset.dropna(subset = ['fun1_1'])
+
+dataset["amb1_1"].isnull().any() #is there any na value 
+dataset["amb1_1"].isnull().sum() #how many -9
+dataset = dataset.dropna(subset = ['amb1_1'])
+
+dataset["shar1_1"].isnull().any() #is there any na value 
+dataset["shar1_1"].isnull().sum() #how many - 22
+dataset = dataset.dropna(subset = ['shar1_1'])
+
+dataset["attr3_1"].isnull().any() #is there any na value 
+dataset["attr3_1"].isnull().sum() #how many - 26
+dataset = dataset.dropna(subset = ['attr3_1'])
+
+dataset.shape #6434, 57
+
+# extract male and female data
+dataset_male = dataset[(dataset["gender"] == 1)]
+dataset_female = dataset[(dataset["gender"] == 0)]
+
+for index, row in dataset_male.iterrows():
+    print(row["pid"])
+
+
 
 # encode categorical data 
 # country
--- speed_dating_experiment/prelim.R
+++
@@ -1,8 +0,0 @@
-setwd("~/Documents/GitHub/MLP/speed_dating_experiment")
-
-dataset = read.csv("Speed Dating Data.csv")
-
-dim(dataset) #8378 x 195 
-
-colnames(dataset)
-unique(dataset$id)
