6283275192f8d66f46608af758f0f1119a114ed0
Toby <Toby@MacBookPro.local>
2017-02-15 13:18:21 +0800

    first version

--- speed_dating_experiment/deep_learning.py
+++ speed_dating_experiment/deep_learning.py
@@ -27,8 +27,7 @@ dataset.shape #8378, 195
 # note:
 
 
-colnames = ["iid", "gender", "wave", "pid", "match", "samerace", "age_o", "race_o", \
-"pf_o_att", "pf_o_sin", "pf_o_int", "pf_o_fun", "pf_o_amb", "pf_o_sha", \
+colnames = ["iid", "gender", "wave", "pid", "match", \
 "age", "field_cd", "race", "imprace", \
 "imprelig", "goal", "date", "go_out", "career_c", \
 "sports", "tvsports", "exercise", "dining", "museums", "art", "hiking", \
@@ -44,92 +43,58 @@ dataset = dataset[colnames]
 # wave values 6 - 9 are in 1-10 scale, with different calculation, remove those rows
 dataset = dataset[(dataset['wave']<6) | (dataset['wave'] > 9)] #6816 rows
 
-# remove row with na value 
-# check is there any na value in pid column
-dataset["pid"].isnull().any()#is there any na value
-dataset["pid"].isnull().sum()# how many - 10
-dataset = dataset.dropna(subset = ['pid']) 
+#rows with na values
+idx = dataset.isnull().any(1).nonzero()[0]
+print (len(idx))
+dataset = dataset.drop(dataset.index[idx]) # drop rows with na values
 
-dataset["age_o"].isnull().any() #is there any na value
-dataset["age_o"].isnull().sum() #how many - 89
-dataset = dataset.dropna(subset = ['age_o'])
+dataset.shape #6567, 48
 
-dataset["pf_o_att"].isnull().any() #is there any na value 
-dataset["pf_o_att"].isnull().sum() #how many - 16
-dataset = dataset.dropna(subset = ['pf_o_att'])
+# change iid and pid as int type
+dataset["iid"] = dataset["iid"].astype(int)
+dataset["pid"] = dataset["pid"].astype(int)
 
-dataset["pf_o_fun"].isnull().any() #is there any na value 
-dataset["pf_o_fun"].isnull().sum() #how many - 9
-dataset = dataset.dropna(subset = ['pf_o_fun'])
-
-dataset["pf_o_sha"].isnull().any() #is there any na value 
-dataset["pf_o_sha"].isnull().sum() #how many -22
-dataset = dataset.dropna(subset = ['pf_o_sha'])
-
-dataset["age"].isnull().any() #is there any na value 
-dataset["age"].isnull().sum() #how many -89
-dataset = dataset.dropna(subset = ['age'])
-
-dataset["field_cd"].isnull().any() #is there any na value 
-dataset["field_cd"].isnull().sum() #how many - 18
-dataset = dataset.dropna(subset = ['field_cd'])
-
-dataset["imprace"].isnull().any() #is there any na value 
-dataset["imprace"].isnull().sum() #how many - 16
-dataset = dataset.dropna(subset = ['imprace'])
-
-dataset["date"].isnull().any() #is there any na value 
-dataset["date"].isnull().sum() #how many - 18
-dataset = dataset.dropna(subset = ['date'])
-
-dataset["career_c"].isnull().any() #is there any na value 
-dataset["career_c"].isnull().sum() #how many - 38
-dataset = dataset.dropna(subset = ['career_c'])
-
-dataset["fun1_1"].isnull().any() #is there any na value 
-dataset["fun1_1"].isnull().sum() #how many -9
-dataset = dataset.dropna(subset = ['fun1_1'])
-
-dataset["amb1_1"].isnull().any() #is there any na value 
-dataset["amb1_1"].isnull().sum() #how many -9
-dataset = dataset.dropna(subset = ['amb1_1'])
-
-dataset["shar1_1"].isnull().any() #is there any na value 
-dataset["shar1_1"].isnull().sum() #how many - 22
-dataset = dataset.dropna(subset = ['shar1_1'])
-
-dataset["attr3_1"].isnull().any() #is there any na value 
-dataset["attr3_1"].isnull().sum() #how many - 26
-dataset = dataset.dropna(subset = ['attr3_1'])
-
-dataset.shape #6434, 57
 
 # extract male and female data
 dataset_male = dataset[(dataset["gender"] == 1)]
+dataset_male = dataset_male.drop_duplicates()
 dataset_female = dataset[(dataset["gender"] == 0)]
+dataset_female = dataset_female.drop_duplicates()
+# rename female dataset columns
+dataset_female.columns = [i + "_F" for i in colnames ]
 
-for index, row in dataset_male.iterrows():
-    print(row["pid"])
 
+dataset_final = pd.DataFrame()
 
+# merge dataset
+for idx in dataset_male.index.values:
+    male_info = dataset.loc[idx, :]
+    male_iid = male_info["iid"].astype(int)
+    female_pid = male_info["pid"].astype(int)
+    rowIdx = ((dataset_female.iid_F == female_pid) & (dataset_female.pid_F == male_iid))
+    
+    # if exists
+    if (sum(rowIdx) > 0):
+        rowIdx2 = dataset_female[rowIdx].index[0]
+        female_info = dataset_female.loc[rowIdx2, :]
+        
+        # pair up male and female
+        combined_row = male_info.append(female_info)
+        # append to dataset_final
+        dataset_final = dataset_final.append(combined_row, ignore_index=True)
 
-# encode categorical data 
-# country
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X_1 = LabelEncoder()
-X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
-# gender
-labelencoder_X_2 = LabelEncoder()
-X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+# drop irrevlant columns
+drop_colnames = ["iid", "iid_F", "wave", "wave_F", "match_F", "pid", "pid_F", "gender", "gender_F"]
+dataset_final = dataset_final.drop(labels=drop_colnames, axis=1)
 
-# create dummy variables for country X X X
-onehotencoder = OneHotEncoder(categorical_features=[1])
-X = onehotencoder.fit_transform(X).toarray()
-X = X[:, 1:] # avoid dummy variable trap, remove 1 dummy variable column
+dataset_final.shape #3167, 87
+
+X = dataset_final.drop(labels="match", axis=1)
+y = dataset_final["match"].astype(int)
 
 # split into training and testing dataset
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state=0) # 25% for testing dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state=0) # 20% for testing dataset
 
 # re-scale feature values
 from sklearn.preprocessing import StandardScaler
@@ -147,13 +112,18 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # add the input layer & 1st hidden layer
-classifier.add(Dense(output_dim=6,
+classifier.add(Dense(output_dim=44,
                      init="uniform",
                      activation="relu",
-                     input_dim=11)) #set input_dim for the 1st layer only
+                     input_dim=86)) #set input_dim for the 1st layer only
 
 # add 2nd hidden layer
-classifier.add(Dense(output_dim=6,
+classifier.add(Dense(output_dim=44,
+                     init="uniform",
+                     activation="relu"))
+
+# add 3rd hidden layer
+classifier.add(Dense(output_dim=44,
                      init="uniform",
                      activation="relu"))
                      
@@ -177,10 +147,10 @@ classifier.fit(X_train, y_train, batch_size=10, nb_epoch=100)
 
 # save the model
 from keras.models import load_model
-classifier.save("nn_model.h5")
-# load the model
-del classifier
-classifier = load_model("nn_model.h5")
+#classifier.save("model.h5")
+## load the model
+#del classifier
+#classifier = load_model("model.h5")
 
 # predict
 y_pred = classifier.predict(X_test) #probabilty
